a9aadeac84f43f67dcc028b99d02d8a9ee81f454
User <pachenkomih@gmail.com>
2023-11-19 02:10:17 +0200

    Move player how in game on the web page

---
+++ Lection 4/Task 2/webserver.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+
+app = Flask("Game")
+
+pos = "0:0"
+
+@app.route("/")
+def index():
+    return render_template("index.html", position = pos)
+
+@app.route("/move")
+def moveChar():
+    global pos
+
+    move = request.args.get("move")
+    currentPosition = request.args.get("currentPosition")
+    pos = currentPosition
+    return render_template("index.html", position = getNewPosition(move, currentPosition))
+
+def getNewPosition(move, currentPosition):
+    currentPositionList = currentPosition.split(":")
+    currentPositionList = list(map(lambda x: int(x), currentPositionList))
+
+    match move:
+        case "R":
+            currentPositionList[0] += 1
+        case "Up":
+            currentPositionList[1] -= 1
+        case "L":
+            currentPositionList[0] -= 1
+        case "Down":
+            currentPositionList[1] += 1
+    
+    if checkPosition(currentPositionList) == False:
+        currentPositionList = list(map(lambda x: str(x), currentPositionList))
+        return ":".join(currentPositionList)
+    else:
+        return pos
+
+def checkPosition(currentPositionList):
+    if currentPositionList[0] == -1 or currentPositionList[1] == -1 or currentPositionList[0] == 3 or currentPositionList[1] == 3:
+        return True
+    else:
+        return False
+
+app.run(host="0.0.0.0", port=8080)
